09f9c9652dd8301c8109e0d392fb30503a8d5550
Borna <borna.grilec@speck.agency>
2020-04-13 14:34:34 +0200

    Added events with mocked data

---
+++ src/lib/events.js
@@ -0,0 +1,52 @@
+const events = [
+    {
+        title: 'How can we benefit from React Redux',
+        location: 'Dvorana D09',
+        dateTime: '24.3. u 13:45h',
+        about:
+            'Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one.',
+        link: '/',
+    },
+    {
+        title: 'JSX means JavaScript and XML',
+        location: 'Dvorana D09',
+        dateTime: '24.3. u 13:45h',
+        about:
+            'Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one.',
+        link: '/',
+    },
+    {
+        title: 'Why to use Styled Components with React',
+        location: 'Dvorana D09',
+        dateTime: '24.3. u 13:45h',
+        about:
+            'Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one.',
+        link: '/',
+    },
+    {
+        title: 'What is the difference between Functional and Class components',
+        location: 'Dvorana D09',
+        dateTime: '24.3. u 13:45h',
+        about:
+            'Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one.',
+        link: '/',
+    },
+    {
+        title: 'Components and Lifecycle methods',
+        location: 'Dvorana D09',
+        dateTime: '24.3. u 13:45h',
+        about:
+            'Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one.',
+        link: '/',
+    },
+    {
+        title: 'Hooks, Mobx, context API in React',
+        location: 'Dvorana D09',
+        dateTime: '24.3. u 13:45h',
+        about:
+            'Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one.',
+        link: '/',
+    },
+];
+
+export default events;
--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,63 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import eventsMock from '../lib/events';
 
 // Components
 import PageTitle from '../components/PageTitle/PageTitle';
 import SectionGrid from '../components/SectionGrid/SectionGrid';
 import InfoBox from '../components/InfoBox/InfoBox';
+import Loader from '../components/Loader/Loader';
 
 const Events = () => {
+    const [events, setEvents] = useState('');
+
+    useEffect(() => {
+        setTimeout(() => {
+            setEvents(eventsMock);
+        }, 1000)
+    }, []);
+
     return (
         <>
             <PageTitle>Events</PageTitle>
             <SectionGrid>
-                <InfoBox
-                    title="How can we benefit from React Redux"
-                    location="Dvorana D09"
-                    dateTime="24.3. u 13:45h"
-                    about="Latin literature from 45 BC, making it over 2000 years old. Richard
-                    McClintock, a Latin professor at
-                    Hampden-Sydney College in Virginia, looked up one."
-                />
-                <InfoBox
-                    title="JSX means JavaScript and XML"
-                    location="Dvorana D09"
-                    dateTime="24.3. u 13:45h"
-                    about="Latin literature from 45 BC, making it over 2000 years old. Richard
-                    McClintock, a Latin professor at
-                    Hampden-Sydney College in Virginia, looked up one."
-                />
-                <InfoBox
-                    title="Why to use Styled Components with React"
-                    location="Dvorana D09"
-                    dateTime="24.3. u 13:45h"
-                    about="Latin literature from 45 BC, making it over 2000 years old. Richard
-                    McClintock, a Latin professor at
-                    Hampden-Sydney College in Virginia, looked up one."
-                />
-                <InfoBox
-                    title="What is the difference between Functional and Class components"
-                    location="Dvorana D09"
-                    dateTime="24.3. u 13:45h"
-                    about="Latin literature from 45 BC, making it over 2000 years old. Richard
-                    McClintock, a Latin professor at
-                    Hampden-Sydney College in Virginia, looked up one."
-                />
-                <InfoBox
-                    title="Components and Lifecycle methods"
-                    location="Dvorana D09"
-                    dateTime="24.3. u 13:45h"
-                    about="Latin literature from 45 BC, making it over 2000 years old. Richard
-                    McClintock, a Latin professor at
-                    Hampden-Sydney College in Virginia, looked up one."
-                />
-                <InfoBox
-                    title="Hooks, Mobx, context API in React"
-                    location="Dvorana D09"
-                    dateTime="24.3. u 13:45h"
-                    about="Latin literature from 45 BC, making it over 2000 years old. Richard
-                    McClintock, a Latin professor at
-                    Hampden-Sydney College in Virginia, looked up one."
-                />
+                {events ? (
+                    events.map((event, index) => (
+                        <InfoBox
+                            key={index}
+                            title={event.title}
+                            location={event.location}
+                            dateTime={event.dateTime}
+                            about={event.about}
+                        />)
+                    )
+                ) : (<Loader />)
+                }
             </SectionGrid>
         </>
     );
