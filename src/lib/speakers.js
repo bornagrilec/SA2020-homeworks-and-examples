b18f517fc486e7a58794faeb6ba60dac8bac818f
Borna <borna.grilec@speck.agency>
2020-04-13 14:34:23 +0200

    Added Speakers with mocked data

---
+++ src/lib/speakers.js
@@ -0,0 +1,40 @@
+const speakers = [
+    {
+        title: 'Johan Bach',
+        about:
+            'Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one.',
+        link: '/',
+    },
+    {
+        title: 'John Cobra',
+        about:
+            'Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one.',
+        link: '/',
+    },
+    {
+        title: 'Miro Slav',
+        about:
+            'Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one.',
+        link: '/',
+    },
+    {
+        title: 'Dinco Dan',
+        about:
+            'Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one.',
+        link: '/',
+    },
+    {
+        title: 'Johana Gun',
+        about:
+            'Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one.',
+        link: '/',
+    },
+    {
+        title: 'Unknown Artist',
+        about:
+            'Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one.',
+        link: '/',
+    },
+];
+
+export default speakers;
--- src/pages/Speakers.js
+++ src/pages/Speakers.js
@@ -1,57 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import speakersMock from '../lib/speakers';
 
 // Components
 import PageTitle from '../components/PageTitle/PageTitle';
 import SectionGrid from '../components/SectionGrid/SectionGrid';
 import InfoBox from '../components/InfoBox/InfoBox';
+import Loader from '../components/Loader/Loader';
 
 const Speakers = () => {
+    const [speakers, setSpeakers] = useState('');
+
+    useEffect(() => {
+        setTimeout(() => {
+            setSpeakers(speakersMock);
+        }, 1000)
+    }, []);
+
     return (
         <>
             <PageTitle>Speakers</PageTitle>
             <SectionGrid>
-                <InfoBox
-                    title="Jochan Bach"
-                    about="Latin literature from 45 BC, making it over 2000 years old. Richard
-                    McClintock, a Latin professor at
-                    Hampden-Sydney College in Virginia, looked up one."
-                    typeSpeakers
-                />
-                <InfoBox
-                    title="John Cobra"
-                    about="Latin literature from 45 BC, making it over 2000 years old. Richard
-                    McClintock, a Latin professor at
-                    Hampden-Sydney College in Virginia, looked up one."
-                    typeSpeakers
-                />
-                <InfoBox
-                    title="Miro Slav"
-                    about="Latin literature from 45 BC, making it over 2000 years old. Richard
-                    McClintock, a Latin professor at
-                    Hampden-Sydney College in Virginia, looked up one."
-                    typeSpeakers
-                />
-                <InfoBox
-                    title="Dinco Dan"
-                    about="Latin literature from 45 BC, making it over 2000 years old. Richard
-                    McClintock, a Latin professor at
-                    Hampden-Sydney College in Virginia, looked up one."
-                    typeSpeakers
-                />
-                <InfoBox
-                    title="Johana Gun"
-                    about="Latin literature from 45 BC, making it over 2000 years old. Richard
-                    McClintock, a Latin professor at
-                    Hampden-Sydney College in Virginia, looked up one."
-                    typeSpeakers
-                />
-                <InfoBox
-                    title="Unknown Artist"
-                    about="Latin literature from 45 BC, making it over 2000 years old. Richard
-                    McClintock, a Latin professor at
-                    Hampden-Sydney College in Virginia, looked up one."
-                    typeSpeakers
-                />
+                {speakers ? (
+                    speakers.map((speaker, index) => (
+                        <InfoBox
+                            key={index}
+                            title={speaker.title}
+                            about={speaker.about}
+                            typeSpeakers
+                        />
+                    ))
+                ) : (<Loader />)}
             </SectionGrid>
         </>
     );
