b24cab4812857bbf54d6422c59315a42ee275f5d
Borna <borna.grilec@speck.agency>
2020-04-13 18:32:12 +0200

    Added filtering using SearchBar

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -6,19 +6,32 @@ import PageTitle from '../components/PageTitle/PageTitle';
 import SectionGrid from '../components/SectionGrid/SectionGrid';
 import InfoBox from '../components/InfoBox/InfoBox';
 import Loader from '../components/Loader/Loader';
+import SearchBar from '../components/SearchBar/SearchBar';
 
 const Events = () => {
     const [events, setEvents] = useState('');
+    const [filteredEvents, setFilteredEvents] = useState('');
 
     useEffect(() => {
         setTimeout(() => {
             setEvents(eventsMock);
+            setFilteredEvents(eventsMock);
         }, 1000)
     }, []);
+
+    const handleSearch = (value) => {
+        const filteredResult = filteredEvents.filter(event => event.title.toLowerCase().includes(value.toLowerCase()));
+        setEvents(filteredResult);
+    }
 
     return (
         <>
             <PageTitle>Events</PageTitle>
+            <SearchBar
+                placeholder="Search events..."
+                disabled={events ? false : true}
+                onValueChange={handleSearch}
+            />
             <SectionGrid>
                 {events ? (
                     events.map((event, index) => (
@@ -28,6 +41,7 @@ const Events = () => {
                             location={event.location}
                             dateTime={event.dateTime}
                             about={event.about}
+                            btnText="Register for the event"
                         />)
                     )
                 ) : (<Loader />)
--- src/pages/Speakers.js
+++ src/pages/Speakers.js
@@ -6,19 +6,39 @@ import PageTitle from '../components/PageTitle/PageTitle';
 import SectionGrid from '../components/SectionGrid/SectionGrid';
 import InfoBox from '../components/InfoBox/InfoBox';
 import Loader from '../components/Loader/Loader';
+import SearchBar from '../components/SearchBar/SearchBar';
 
 const Speakers = () => {
+    // We need to have initial states for speakers and filteredSpeakers.
+    // This is because if we filter initial state of speakers, our array of object
+    // will exclude items from the original array according to serach result. 
+    // On that way, when you try to clear search bar, items wouldn't be rendered
+    // like it should. Solution is to create initial state for filteredSpeakers
+    // which will be copy of original speakers array when first render occured.
+    // Then, when we searching, we must use filteredArray and never set filteredSpeakers state
+    // again, just change state of speakers original array during each handleSearch call.
     const [speakers, setSpeakers] = useState('');
+    const [filteredSpeakers, setFilteredSpeakers] = useState('');
 
     useEffect(() => {
         setTimeout(() => {
             setSpeakers(speakersMock);
+            setFilteredSpeakers(speakersMock);
         }, 1000)
     }, []);
+
+    const handleSearch = (value) => {
+        const filteredResult = filteredSpeakers.filter(speaker => speaker.title.toLowerCase().includes(value.toLowerCase()));
+        setSpeakers(filteredResult);
+    }
 
     return (
         <>
             <PageTitle>Speakers</PageTitle>
+            <SearchBar
+                placeholder="Search speakers..."
+                disabled={speakers ? false : true}
+                onValueChange={handleSearch} />
             <SectionGrid>
                 {speakers ? (
                     speakers.map((speaker, index) => (
@@ -26,6 +46,7 @@ const Speakers = () => {
                             key={index}
                             title={speaker.title}
                             about={speaker.about}
+                            btnText="Follow speaker"
                             typeSpeakers
                         />
                     ))
