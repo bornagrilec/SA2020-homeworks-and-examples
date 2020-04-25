5a7020091dced8fd48fac9126a751bab6c0b3c18
Borna <borna.grilec@speck.agency>
2020-04-25 08:26:04 +0200

    Print data from api inside component

---
+++ src/pages/ToDos.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import { getTodos } from '../services/todos';
+
+const ToDos = () => {
+    const [todos, setTodos] = useState('');
+
+    useEffect(() => {
+        getTodos().then(todos => setTodos(todos));
+    });
+
+    return (
+        <ul>
+            {todos ? todos.map(todo => <li>{todo.title}</li>) : <div>Loading...</div>}
+        </ul>
+    );
+}
+
+export default ToDos;
