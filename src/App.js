ef4b9557efaa6163fb151b2dd27c750de5dda5ee
Borna <borna.grilec@speck.agency>
2020-04-30 12:22:18 +0200

    Added login & register routes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Header from './components/Header/Header';
 import Home from './pages/Home';
 import Events from './pages/Events';
 import Speakers from './pages/Speakers';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 class App extends Component {
   render() {
@@ -19,6 +21,8 @@ class App extends Component {
           <Route exact path="/" component={Home} />
           <Route path="/events" component={Events} />
           <Route path="/speakers" component={Speakers} />
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
         </Main>
       </>
     );
