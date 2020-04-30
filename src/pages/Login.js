212ba95dd8eff3bf155c0df9e8179bbb7f316573
Borna <borna.grilec@speck.agency>
2020-04-30 12:22:06 +0200

    Created Login & Regirster pages

---
+++ src/pages/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { PageTitle } from '../lib/styles/GeneralStyles';
+
+const Login = (props) => {
+    return (
+        <>
+            <PageTitle>Login</PageTitle>
+
+        </>
+    );
+}
+
+export default Login;
---
+++ src/pages/Register.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { PageTitle } from '../lib/styles/GeneralStyles';
+
+const Register = (props) => {
+    return (
+        <>
+            <PageTitle>Registration</PageTitle>
+
+        </>
+    );
+}
+
+export default Register;
